7c52fd8f42bde6f606051dcbb599037d621ab39f
tiberiutudorascu <tiberiutudorascu@gmail.com>
2025-03-30 14:42:32 +0300

    Update void.ino

--- void.ino
+++ void.ino
@@ -1,17 +1,17 @@
-#include <Wire.h>              // Bibliotecă pentru comunicația I2C
-#include <Adafruit_GFX.h>      // Bibliotecă grafică Adafruit
-#include <Adafruit_SSD1306.h>  // Bibliotecă pentru afișajul OLED SSD1306
-#include "images.h"            // Include fișierul cu imagini predefinite
+#include <Wire.h>            
+#include <Adafruit_GFX.h>      
+#include <Adafruit_SSD1306.h>  
+#include "images.h"           
 #include "DHT.h"
-#include "icons.h"  // Bibliotecă pentru senzorul DHT (temperatură și umiditate)
+#include "icons.h"  
 
-#define SCREEN_WIDTH 128     // Lățimea ecranului OLED în pixeli
-#define SCREEN_HEIGHT 64     // Înălțimea ecranului OLED în pixeli
-#define OLED_RESET -1        // Pinul de reset pentru OLED (nu este utilizat, de aceea -1)
-#define SCREEN_ADDRESS 0x3C  // Adresa I2C a ecranului OLED
+#define SCREEN_WIDTH 128    
+#define SCREEN_HEIGHT 64    
+#define OLED_RESET -1        
+#define SCREEN_ADDRESS 0x3C  
 
-#define DHTPIN 17      // Pinul la care este conectat senzorul DHT
-#define DHTTYPE DHT11  // Tipul senzorului DHT utilizat
+#define DHTPIN 17      
+#define DHTTYPE DHT11 
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 DHT dht(DHTPIN, DHTTYPE);
@@ -29,7 +29,7 @@ bool apasat = false;
 bool apasat2 = false;
 enum Mode { IDLE,
             CUTE };
-Mode currentMode = IDLE;  // idle e activ initial
+Mode currentMode = IDLE;  
 
 bool infoBool = false;
 
@@ -147,13 +147,11 @@ void cute(int *health) {
 
 
 void progress() {
-  unsigned long currentTime2 = millis();  // Obține timpul curent pentru animația de progres
-  display.clearDisplay();                 // Curăță ecranul
+  unsigned long currentTime2 = millis();  
+  display.clearDisplay();                 
 
-  // Dacă s-a trecut suficient timp, actualizează animația de progres
   if (currentTime2 - previousTime2 >= 300) {
-    previousTime2 = currentTime2;  // Actualizează timpul ultimei actualizări
-    // Selectează pictograma corespunzătoare în funcție de intervalul de temperatură
+    previousTime2 = currentTime2; 
     if (temperature >= 19 && temperature <= 27)
       display.drawBitmap(0, 0, progiconsSun[currentFrame2], 128, 64, SSD1306_WHITE);
     if (temperature >= 15 && temperature <= 18)
@@ -163,7 +161,6 @@ void progress() {
     if (temperature >= 28 && temperature <= 40)
       display.drawBitmap(0, 0, progiconsHot[currentFrame2], 128, 64, SSD1306_WHITE);
 
-    // Desenează diverse elemente grafice suplimentare pe ecran (probabil reprezentând bare sau indicatori)
     display.drawRoundRect(5, 16, 10, 40, 10, SSD1306_WHITE);
     display.fillRoundRect(5, 46, 10, 10, 10, SSD1306_WHITE);
 
@@ -173,11 +170,10 @@ void progress() {
     display.drawRoundRect(45, 16, 10, 40, 10, SSD1306_WHITE);
     display.drawRoundRect(65, 16, 10, 40, 10, SSD1306_WHITE);
 
-    currentFrame2++;  // Trecerea la următorul cadru pentru animația de progres
+    currentFrame2++;  
     if (currentFrame2 >= 3)
-      currentFrame2 = 0;  // Resetarea cadrului după ce s-au parcurs toate opțiunile de pictograme
+      currentFrame2 = 0;  
   } else {
-    // Desenează elementele grafice fără a actualiza cadrul animației
     display.drawRoundRect(5, 16, 10, 40, 10, SSD1306_WHITE);
     display.fillRoundRect(5, 46, 10, 10, 10, SSD1306_WHITE);
 
@@ -197,47 +193,46 @@ void progress() {
     if (temperature >= 28 && temperature <= 40)
       display.drawBitmap(0, 0, progiconsHot[currentFrame2], 128, 64, SSD1306_WHITE);
   }
-  display.display();  // Actualizează ecranul OLED cu toate modificările
+  display.display();  
 }
 
 void stats() {
-  display.clearDisplay();               // Curăță ecranul înainte de afișarea informațiilor
-  display.setTextColor(SSD1306_WHITE);  // Setează culoarea textului la alb
-  display.setTextSize(1);               // Setează dimensiunea textului
-  display.setCursor(0, 0);              // Setează cursorul în colțul din stânga sus
+  display.clearDisplay();             
+  display.setTextColor(SSD1306_WHITE); 
+  display.setTextSize(1);              
+  display.setCursor(0, 0);              
 
-  // Afișează diferite pagini de informații/statistici în funcție de valoarea lui infoPage
   if (infoPage == 0) {
-    display.println("INFORMATION - Stats");  // Titlu pentru pagina de informații
+    display.println("INFORMATION - Stats");  
     display.setCursor(0, 16);
     display.println("On the next pages you will find general informations and how to keep your Void pet healthy, safe and active.");
     display.setCursor(0, 0);
   } else if (infoPage == 1) {
-    display.println("How to: Health");  // Informații despre sănătate
+    display.println("How to: Health");  
     display.setCursor(0, 16);
     display.println("You can fill up Void's Health bar by giving him a shower and keeping him well fed.");
     display.setCursor(0, 0);
   } else if (infoPage == 2) {
-    display.println("How to: Hunger");  // Informații despre foame
+    display.println("How to: Hunger"); 
     display.setCursor(0, 16);
     display.println("You can fill up Void's Hunger bar by interacting with the human touch sensor. He feeds off souls.");
     display.setCursor(0, 0);
   } else if (infoPage == 3) {
-    display.println("How to: Joy");  // Informații despre bucurie
+    display.println("How to: Joy");  
     display.setCursor(0, 16);
     display.println("You can fill up Void's Joy bar by playing the different minigames. Don't try too hard, he doesn't mind losing.");
     display.setCursor(0, 0);
   } else if (infoPage == 4) {
-    display.println("How to: Sleep");  // Informații despre somn
+    display.println("How to: Sleep");  
     display.setCursor(0, 16);
     display.println("After you're done with everything be sure to lay him to bed. He loves sleeping..Maybe too much.");
     display.setCursor(0, 0);
   } else if (infoPage == 5) {
-    display.println("How to: Climate");  // Informații despre climat
+    display.println("How to: Climate");  
     display.setCursor(0, 16);
     display.println("Be sure the temperature isn't too high or too low. He may be an alien, but he still likes comfort");
     display.setCursor(0, 0);
   }
 
-  display.display();  // Actualizează ecranul OLED pentru a afișa textul
+  display.display();  
 }
