171d66a7a2b4b2ac7cc1e9145f981b83147652fe
tiberiutudorascu <tiberiutudorascu@gmail.com>
2025-04-01 04:12:13 +0300

    Add files via upload

--- icons.h
+++ icons.h
@@ -1,28 +1,34 @@
-const unsigned char *frames[] = { happyidle1, happyidle2, happyidle3, happyidle4, happyidle5, happyidle6,
-                                  happyidle7, happyidle8, happyidle9, happyidle8, happyidle7, happyidle6, happyidle5, happyidle4, happyidle3, happyidle2,
-                                  happyidle1, happyidle1, happyidle2, happyidle3, happyidle4, happyidle5, happyidle6, happyidle7, happyidle8, happyidle9,
-                                  happyidle8, happyidle7, happyidle6, happyidle5, happyidle4, happyidle3, happyidle2, happyidle1, happyidle1, happyidle2,
-                                  happyidle3, happyidle4, happyidle5, happyidle6, happyidle7, happyidle8, happyidle9, happyidle8, happyidle7, happyidle6,
-                                  happyidle5, happyidle4, happyidle3, happyidle2, happyidle1, happyidle10, happyidle11, happyidle12, happyidle13, happyidle14,
-                                  happyidle13, happyidle12, happyidle11, happyidle10, happyidle1 };
-const unsigned char *frames3[] = { cute1, cute2, cute3, cute4, cute5,cute6,cute7,cute6,cute5,cute4,cute3,cute2,cute1};
+const unsigned char *frames[] = {happyidle1, happyidle2, happyidle3, happyidle4, happyidle5, happyidle6,
+                                 happyidle7, happyidle8, happyidle9, happyidle8, happyidle7, happyidle6, happyidle5, happyidle4, happyidle3, happyidle2,
+                                 happyidle1, happyidle1, happyidle2, happyidle3, happyidle4, happyidle5, happyidle6, happyidle7, happyidle8, happyidle9,
+                                 happyidle8, happyidle7, happyidle6, happyidle5, happyidle4, happyidle3, happyidle2, happyidle1, happyidle1, happyidle2,
+                                 happyidle3, happyidle4, happyidle5, happyidle6, happyidle7, happyidle8, happyidle9, happyidle8, happyidle7, happyidle6,
+                                 happyidle5, happyidle4, happyidle3, happyidle2, happyidle1, happyidle10, happyidle11, happyidle12, happyidle13, happyidle14,
+                                 happyidle13, happyidle12, happyidle11, happyidle10, happyidle1};
+const unsigned char *frames3[] = {cute1, cute2, cute3, cute4, cute5, cute6, cute7, cute6, cute5, cute4, cute3, cute2, cute1};
+const unsigned char *frames4[] = {angry1, angry2, angry3, angry4, angry5, angry6, angry7, angry8, angry7, angry8, angry7, angry8, angry7, angry8, angry7, angry8, angry7, angry8, angry6, angry5, angry4, angry3, angry2, angry1};
 
-const unsigned char *progiconsSun[] = { infosun1, infosun2, infosun3 };
-const unsigned char *progiconsCold[] = { infocold1, infocold2, infocold3 };
-const unsigned char *progiconsHot[] = { infohot1, infohot2, infohot3 };
-const unsigned char *progiconsFreeze[] = { infofreeze1, infofreeze2, infofreeze3 };
+const unsigned char *progiconsSun[] = {infosun1, infosun2, infosun3};
+const unsigned char *progiconsCold[] = {infocold1, infocold2, infocold3};
+const unsigned char *progiconsHot[] = {infohot1, infohot2, infohot3};
+const unsigned char *progiconsFreeze[] = {infofreeze1, infofreeze2, infofreeze3};
 
-const unsigned framestime[] = { 10, 10, 10, 10, 10, 10, 10, 10, 30, 10, 10, 10, 10, 10, 10, 10, 5000,
-                                10, 10, 10, 10, 10, 10, 10, 10, 30, 10, 10, 10, 10, 10, 10, 10, 5000,
-                                10, 10, 10, 10, 10, 10, 10, 10, 30, 10, 10, 10, 10, 10, 10, 10, 5000,
-                                15, 15, 15, 15, 3000, 15, 15, 15, 15};
-const unsigned framestime3[] = { 30, 30, 30, 30, 30, 30, 2000,5000,30,30,30,30,30,30};
+const unsigned framestime[] = {10, 10, 10, 10, 10, 10, 10, 10, 30, 10, 10, 10, 10, 10, 10, 10, 5000,
+                               10, 10, 10, 10, 10, 10, 10, 10, 30, 10, 10, 10, 10, 10, 10, 10, 5000,
+                               10, 10, 10, 10, 10, 10, 10, 10, 30, 10, 10, 10, 10, 10, 10, 10, 5000,
+                               15, 15, 15, 15, 3000, 15, 15, 15, 15};
+const unsigned framestime3[] = {10, 10, 10, 10, 10, 10, 2000, 5000, 10, 10, 10, 10, 10, 10};
+const unsigned int framestime4[] = {20, 20, 20, 20, 50, 200, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 200, 100, 80, 50, 40, 30, 20, 20};
 const int totalFrames = sizeof(frames) / sizeof(frames[0]);
 const int totalFrames3 = sizeof(frames3) / sizeof(frames3[0]);
+const int totalFrames4 = sizeof(frames4) / sizeof(frames4[0]);
 
-unsigned long previousTime = 0;                              
-unsigned long previousTime2 = 0;                            
+unsigned long previousTime = 0;
+unsigned long previousTime2 = 0;
 unsigned long previousTime3 = 0;
-int currentFrame = 0; 
-int currentFrame2 = 0;
-int currentFrame3 = 0;  
+unsigned long previousTime4 = 0;
+
+int currentFrames = 0;
+int currentFrames2 = 0;
+int currentFrames3 = 0;
+int currentFrames4 = 0;
--- void.ino
+++ void.ino
@@ -26,9 +26,13 @@ int buttonPressCount = 0;
 
 bool apasat = false;
 bool apasat2 = false;
-enum Mode { IDLE,
-            CUTE };
-Mode currentMode = IDLE;  // idle e activ initial
+enum Mode
+{
+    IDLE,
+    CUTE,
+    ANGRY
+};
+Mode currentMode = IDLE; // idle e activ initial
 
 bool infoBool = false;
 
@@ -37,208 +41,287 @@ int totalInfoPages = 5;
 
 float temperature;
 int healthpoints = 0;
+float angrypoints = 0;
 
-void setup() {
-  Serial.begin(9600);
-  dht.begin();
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("Eroare la initializarea OLED!"));
-    for (;;)
-      ;
-  }
-  pinMode(4, INPUT_PULLUP);
-  pinMode(16, INPUT_PULLUP);
-  display.clearDisplay();
-  display.display();
+void setup()
+{
+    Serial.begin(9600);
+    dht.begin();
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    {
+        Serial.println(F("Eroare la initializarea OLED!"));
+        for (;;)
+            ;
+    }
+    pinMode(4, INPUT_PULLUP);
+    pinMode(16, INPUT_PULLUP);
+    display.clearDisplay();
+    display.display();
 }
 
-void loop() {
-  unsigned long currentMillis = millis();
-  temperature = dht.readTemperature();
+void loop()
+{
+    unsigned long currentMillis = millis();
+    temperature = dht.readTemperature();
 
-  if (digitalRead(4) == LOW && apasat && (currentMillis - lastButtonPress1 > debounceDelay)) {
-    apasat = false;
-    lastButtonPress1 = currentMillis;
-    if (infoBool) infoPage++;
-    else page++;
-    if (page > totalPages) page = 0;
-    if (infoPage > totalInfoPages) infoPage = 0;
-  }
-  if (digitalRead(4) == HIGH) apasat = true;
+    if (digitalRead(4) == LOW && apasat && (currentMillis - lastButtonPress1 > debounceDelay))
+    {
+        apasat = false;
+        lastButtonPress1 = currentMillis;
+        if (infoBool)
+            infoPage++;
+        else
+            page++;
+        if (page > totalPages)
+            page = 0;
+        if (infoPage > totalInfoPages)
+            infoPage = 0;
+    }
+    if (digitalRead(4) == HIGH)
+        apasat = true;
 
-  if (digitalRead(16) == LOW && apasat2 && (currentMillis - lastButtonPress2 > debounceDelay)) {
-    apasat2 = false;
-    lastButtonPress2 = currentMillis;
+    if (digitalRead(16) == LOW && apasat2 && (currentMillis - lastButtonPress2 > debounceDelay))
+    {
+        apasat2 = false;
+        lastButtonPress2 = currentMillis;
 
-    if (page == 1) {
-      infoBool = !infoBool;
-      if (!infoBool) previousTime2 = currentMillis;
+        if (page == 1)
+        {
+            infoBool = !infoBool;
+            if (!infoBool)
+                previousTime2 = currentMillis;
+        }
+
+        else if (page == 0)
+        {
+            buttonPressCount++;
+            Serial.println(buttonPressCount);
+            Serial.print("AngryPoints: ");
+            Serial.println(angrypoints);
+
+            if (buttonPressCount == 15 && angrypoints == 3)
+            {
+                currentMode = ANGRY;
+                buttonPressCount = 0;
+            }
+
+            else if (buttonPressCount == 15 && angrypoints < 3)
+            {
+                currentMode = CUTE;
+                angrypoints = angrypoints + 0.5;
+                Serial.print("Health: ");
+                Serial.println(healthpoints);
+
+                buttonPressCount = 0;
+            }
+        }
     }
 
-    else if (page == 0) {
-      buttonPressCount++;
-      Serial.println(buttonPressCount);
-      if (buttonPressCount == 15) {
-        currentMode = CUTE;
-        Serial.print("Health: ");
-        Serial.println(healthpoints);
-        buttonPressCount = 0;
-      }
+    if (digitalRead(16) == HIGH)
+        apasat2 = true;
+
+    static int lastPage = -1;
+    if (page != lastPage)
+    {
+        Serial.print("Pagina curenta: ");
+        Serial.println(page);
+        lastPage = page;
     }
-  }
 
-  if (digitalRead(16) == HIGH) apasat2 = true;
+    if (page == 0)
+    {
+        if (currentMode == IDLE)
+            idle();
+        else if (currentMode == CUTE)
+            cute(healthpoints);
+        else if (currentMode == ANGRY)
+            angry(healthpoints);
+    }
+    else if (page == 1)
+    {
+        if (infoBool)
+            stats();
+        else
+            progress();
+    }
+    else if (page == 2 || page == 3)
+    {
+        display.clearDisplay();
+        display.display();
+    }
 
+    if (currentMillis - lastTempCheck >= 10000)
+    {
+        Serial.println(temperature);
+        lastTempCheck = currentMillis;
+    }
+}
 
+void idle()
+{
+    unsigned long currentTime = millis();
+    if (currentTime - previousTime >= framestime[currentFrames])
+    {
+        previousTime = currentTime;
+        display.clearDisplay();
+        display.drawBitmap(0, 0, frames[currentFrames], 128, 64, SSD1306_WHITE);
+        display.display();
+        currentFrames++;
+        if (currentFrames >= totalFrames)
+            currentFrames = 0;
+    }
+}
 
-  static int lastPage = -1;
-  if (page != lastPage) {
-    Serial.print("Pagina curenta: ");
-    Serial.println(page);
-    lastPage = page;
-  }
+void cute(int &health)
+{
+    unsigned long currentTime3 = millis();
 
-  if (page == 0) {
-    if (currentMode == IDLE)
-      idle();
-    else if (currentMode == CUTE) cute(healthpoints);
-  } else if (page == 1) {
-    if (infoBool) stats();
-    else progress();
-  } else if (page == 2 || page == 3) {
+    Serial.print("Frame: ");
+    Serial.print(currentFrames3);
+    Serial.print(" / ");
+    Serial.println(totalFrames3);
+
+    if (currentTime3 - previousTime3 >= framestime3[currentFrames3])
+    {
+        previousTime3 = currentTime3;
+        display.clearDisplay();
+        display.drawBitmap(0, 0, frames3[currentFrames3], 128, 64, SSD1306_WHITE);
+        display.display();
+        currentFrames3++;
+    }
+
+    if (currentFrames3 >= totalFrames3)
+    {
+        Serial.println("ANIMATIE TERMINATA - +1 HEALTH");
+        health++;
+        Serial.print("HEALTH: ");
+        Serial.println(health);
+        currentMode = IDLE;
+        currentFrames3 = 0;
+    }
+}
+
+void angry(int &health)
+{
+    unsigned long currentTime4 = millis();
+    if (currentTime4 - previousTime4 >= framestime4[currentFrames4])
+    {
+        previousTime4 = currentTime4;
+        display.clearDisplay();
+        display.drawBitmap(0, 0, frames4[currentFrames4], 128, 64, SSD1306_WHITE);
+        display.display();
+        currentFrames4++;
+        if (currentFrames4 >= totalFrames4)
+        {
+            currentFrames4 = 0;
+            angrypoints = 0;
+            health--;
+            currentMode = IDLE;
+        }
+    }
+}
+void progress()
+{
+    unsigned long currentTime2 = millis();
     display.clearDisplay();
-    display.display();
-  }
 
-  if (currentMillis - lastTempCheck >= 10000) {
-    Serial.println(temperature);
-    lastTempCheck = currentMillis;
-  }
+    if (currentTime2 - previousTime2 >= 300)
+    {
+        previousTime2 = currentTime2;
+        if (temperature >= 19 && temperature <= 27)
+            display.drawBitmap(0, 0, progiconsSun[currentFrames2], 128, 64, SSD1306_WHITE);
+        if (temperature >= 15 && temperature <= 18)
+            display.drawBitmap(0, 0, progiconsCold[currentFrames2], 128, 64, SSD1306_WHITE);
+        if (temperature >= -5 && temperature <= 14)
+            display.drawBitmap(0, 0, progiconsFreeze[currentFrames2], 128, 64, SSD1306_WHITE);
+        if (temperature >= 28 && temperature <= 40)
+            display.drawBitmap(0, 0, progiconsHot[currentFrames2], 128, 64, SSD1306_WHITE);
+
+        display.drawRoundRect(5, 16, 10, 40, 10, SSD1306_WHITE);
+        display.fillRoundRect(5, 46, 10, 10, 10, SSD1306_WHITE);
+
+        display.drawRoundRect(25, 16, 10, 40, 10, SSD1306_WHITE);
+        display.drawRoundRect(25, 16, 10, 40, 10, SSD1306_WHITE);
+
+        display.drawRoundRect(45, 16, 10, 40, 10, SSD1306_WHITE);
+        display.drawRoundRect(65, 16, 10, 40, 10, SSD1306_WHITE);
+
+        currentFrames2++;
+        if (currentFrames2 >= 3)
+            currentFrames2 = 0;
+    }
+    else
+    {
+        display.drawRoundRect(5, 16, 10, 40, 10, SSD1306_WHITE);
+        display.fillRoundRect(5, 46, 10, 10, 10, SSD1306_WHITE);
+
+        display.drawRoundRect(25, 16, 10, 40, 10, SSD1306_WHITE);
+        display.drawRoundRect(25, 16, 10, 40, 10, SSD1306_WHITE);
+
+        display.drawRoundRect(45, 16, 10, 40, 10, SSD1306_WHITE);
+        display.drawRoundRect(65, 16, 10, 40, 10, SSD1306_WHITE);
+
+        if (temperature >= 19 && temperature <= 27)
+            display.drawBitmap(0, 0, progiconsSun[currentFrames2], 128, 64, SSD1306_WHITE);
+        if (temperature >= 15 && temperature <= 18)
+            display.drawBitmap(0, 0, progiconsCold[currentFrames2], 128, 64, SSD1306_WHITE);
+        if (temperature >= -5 && temperature <= 14)
+            display.drawBitmap(0, 0, progiconsFreeze[currentFrames2], 128, 64, SSD1306_WHITE);
+        if (temperature >= 28 && temperature <= 40)
+            display.drawBitmap(0, 0, progiconsHot[currentFrames2], 128, 64, SSD1306_WHITE);
+    }
+    display.display();
 }
 
-void idle() {
-  unsigned long currentTime = millis();
-  if (currentTime - previousTime >= framestime[currentFrame]) {
-    previousTime = currentTime;
+void stats()
+{
     display.clearDisplay();
-    display.drawBitmap(0, 0, frames[currentFrame], 128, 64, SSD1306_WHITE);
+    display.setTextColor(SSD1306_WHITE);
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+
+    if (infoPage == 0)
+    {
+        display.println("INFORMATION - Stats");
+        display.setCursor(0, 16);
+        display.println("On the next pages you will find general informations and how to keep your Void pet healthy, safe and active.");
+        display.setCursor(0, 0);
+    }
+    else if (infoPage == 1)
+    {
+        display.println("How to: Health");
+        display.setCursor(0, 16);
+        display.println("You can fill up Void's Health bar by giving him a shower and keeping him well fed.");
+        display.setCursor(0, 0);
+    }
+    else if (infoPage == 2)
+    {
+        display.println("How to: Hunger");
+        display.setCursor(0, 16);
+        display.println("You can fill up Void's Hunger bar by interacting with the human touch sensor. He feeds off souls.");
+        display.setCursor(0, 0);
+    }
+    else if (infoPage == 3)
+    {
+        display.println("How to: Joy");
+        display.setCursor(0, 16);
+        display.println("You can fill up Void's Joy bar by playing the different minigames. Don't try too hard, he doesn't mind losing.");
+        display.setCursor(0, 0);
+    }
+    else if (infoPage == 4)
+    {
+        display.println("How to: Sleep");
+        display.setCursor(0, 16);
+        display.println("After you're done with everything be sure to lay him to bed. He loves sleeping..Maybe too much.");
+        display.setCursor(0, 0);
+    }
+    else if (infoPage == 5)
+    {
+        display.println("How to: Climate");
+        display.setCursor(0, 16);
+        display.println("Be sure the temperature isn't too high or too low. He may be an alien, but he still likes comfort");
+        display.setCursor(0, 0);
+    }
+
     display.display();
-    currentFrame++;
-    if (currentFrame >= totalFrames) currentFrame = 0;
-  }
-}
-
-void cute(int &health) {
-  unsigned long currentTime3 = millis();
-
-  Serial.print("Frame: ");
-  Serial.print(currentFrame3);
-  Serial.print(" / ");
-  Serial.println(totalFrames3);
-
-  if (currentTime3 - previousTime3 >= framestime3[currentFrame3]) {
-    previousTime3 = currentTime3;
-    display.clearDisplay();
-    display.drawBitmap(0, 0, frames3[currentFrame3], 128, 64, SSD1306_WHITE);
-    display.display();
-    currentFrame3++;
-  }
-
-  if (currentFrame3 >= totalFrames3) {
-    Serial.println("ANIMATIE TERMINATA - +1 HEALTH");
-    health++;
-    Serial.print("HEALTH: ");
-    Serial.println(health);
-    currentMode = IDLE;
-    currentFrame3 = 0;
-  }
-}
-void progress() {
-  unsigned long currentTime2 = millis();
-  display.clearDisplay();
-
-  if (currentTime2 - previousTime2 >= 300) {
-    previousTime2 = currentTime2;
-    if (temperature >= 19 && temperature <= 27)
-      display.drawBitmap(0, 0, progiconsSun[currentFrame2], 128, 64, SSD1306_WHITE);
-    if (temperature >= 15 && temperature <= 18)
-      display.drawBitmap(0, 0, progiconsCold[currentFrame2], 128, 64, SSD1306_WHITE);
-    if (temperature >= -5 && temperature <= 14)
-      display.drawBitmap(0, 0, progiconsFreeze[currentFrame2], 128, 64, SSD1306_WHITE);
-    if (temperature >= 28 && temperature <= 40)
-      display.drawBitmap(0, 0, progiconsHot[currentFrame2], 128, 64, SSD1306_WHITE);
-
-    display.drawRoundRect(5, 16, 10, 40, 10, SSD1306_WHITE);
-    display.fillRoundRect(5, 46, 10, 10, 10, SSD1306_WHITE);
-
-    display.drawRoundRect(25, 16, 10, 40, 10, SSD1306_WHITE);
-    display.drawRoundRect(25, 16, 10, 40, 10, SSD1306_WHITE);
-
-    display.drawRoundRect(45, 16, 10, 40, 10, SSD1306_WHITE);
-    display.drawRoundRect(65, 16, 10, 40, 10, SSD1306_WHITE);
-
-    currentFrame2++;
-    if (currentFrame2 >= 3)
-      currentFrame2 = 0;
-  } else {
-    display.drawRoundRect(5, 16, 10, 40, 10, SSD1306_WHITE);
-    display.fillRoundRect(5, 46, 10, 10, 10, SSD1306_WHITE);
-
-    display.drawRoundRect(25, 16, 10, 40, 10, SSD1306_WHITE);
-    display.drawRoundRect(25, 16, 10, 40, 10, SSD1306_WHITE);
-
-    display.drawRoundRect(45, 16, 10, 40, 10, SSD1306_WHITE);
-    display.drawRoundRect(65, 16, 10, 40, 10, SSD1306_WHITE);
-
-    if (temperature >= 19 && temperature <= 27)
-      display.drawBitmap(0, 0, progiconsSun[currentFrame2], 128, 64, SSD1306_WHITE);
-    if (temperature >= 15 && temperature <= 18)
-      display.drawBitmap(0, 0, progiconsCold[currentFrame2], 128, 64, SSD1306_WHITE);
-    if (temperature >= -5 && temperature <= 14)
-      display.drawBitmap(0, 0, progiconsFreeze[currentFrame2], 128, 64, SSD1306_WHITE);
-    if (temperature >= 28 && temperature <= 40)
-      display.drawBitmap(0, 0, progiconsHot[currentFrame2], 128, 64, SSD1306_WHITE);
-  }
-  display.display();
-}
-
-void stats() {
-  display.clearDisplay();
-  display.setTextColor(SSD1306_WHITE);
-  display.setTextSize(1);
-  display.setCursor(0, 0);
-
-  if (infoPage == 0) {
-    display.println("INFORMATION - Stats");
-    display.setCursor(0, 16);
-    display.println("On the next pages you will find general informations and how to keep your Void pet healthy, safe and active.");
-    display.setCursor(0, 0);
-  } else if (infoPage == 1) {
-    display.println("How to: Health");
-    display.setCursor(0, 16);
-    display.println("You can fill up Void's Health bar by giving him a shower and keeping him well fed.");
-    display.setCursor(0, 0);
-  } else if (infoPage == 2) {
-    display.println("How to: Hunger");
-    display.setCursor(0, 16);
-    display.println("You can fill up Void's Hunger bar by interacting with the human touch sensor. He feeds off souls.");
-    display.setCursor(0, 0);
-  } else if (infoPage == 3) {
-    display.println("How to: Joy");
-    display.setCursor(0, 16);
-    display.println("You can fill up Void's Joy bar by playing the different minigames. Don't try too hard, he doesn't mind losing.");
-    display.setCursor(0, 0);
-  } else if (infoPage == 4) {
-    display.println("How to: Sleep");
-    display.setCursor(0, 16);
-    display.println("After you're done with everything be sure to lay him to bed. He loves sleeping..Maybe too much.");
-    display.setCursor(0, 0);
-  } else if (infoPage == 5) {
-    display.println("How to: Climate");
-    display.setCursor(0, 16);
-    display.println("Be sure the temperature isn't too high or too low. He may be an alien, but he still likes comfort");
-    display.setCursor(0, 0);
-  }
-
-  display.display();
 }
